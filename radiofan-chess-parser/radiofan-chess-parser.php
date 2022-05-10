e0b9722093af5d6d6c69dfbdabb9587447642be5
RADIOFAN <radiofan22@mail.ru>
2022-05-10 10:55:15 +0700

    init commit

---
+++ radiofan-chess-parser/radiofan-chess-parser.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @wordpress-plugin
+ * Plugin Name: RADIOFAN Chess Parser
+ * Plugin URI: https://github.com/radiofan
+ * Version: 0.0
+ * Author: RADIOFAN
+ * Author URI: http://vk.com/radio_fan
+ * Description: todo add desc
+*/
