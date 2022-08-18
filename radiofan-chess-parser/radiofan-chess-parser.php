3a6cd46d6d42adc1daaf6ddf0a6a86aefb6ae2d0
RADIOFAN <radiofan22@mail.ru>
2022-08-18 23:02:38 +0700

    V1.2 Изменения вывода таблицы рейтингов и игроков, возможность выгрузки таблиц рейтингов с динамикой в формате Excel, получение рейтингов разделено на соответсвующие сайты (ruchess и fide)

--- radiofan-chess-parser/radiofan-chess-parser.php
+++ radiofan-chess-parser/radiofan-chess-parser.php
@@ -3,10 +3,10 @@
  * @wordpress-plugin
  * Plugin Name: RADIOFAN Chess Parser
  * Plugin URI: https://github.com/radiofan/radiofan-chess-parser
- * Version: 1.1
+ * Version: 1.2
  * Author: RADIOFAN
  * Author URI: http://vk.com/radio_fan
- * Description: Данный плагин позволяет парсить и данные игроков с сайта <a href="https://ratings.ruchess.ru/api">ratings.ruchess.ru</a>.<br>С помощью шорткода <code>[chess_top_scoreboard list_url=""]</code> можно вывести актуальный блок топа игроков, атрибут list_url может содержать ссылку на список всех игроков.<br>С помощью шорткода <code>[chess_players_page]</code> можно вывести таблицу с поиском, сортировкой и пагинацией всех игроков.<br>Также доступна настройка парсинга по фильтру
+ * Description: Данный плагин позволяет парсить данные игроков с сайта <a href="https://ratings.ruchess.ru/api">ratings.ruchess.ru</a> и  <a href="https://ratings.fide.com/download_lists.phtml>ratings.fide.com</a>. С помощью шорткода <code>[chess_top_scoreboard list_url=""]</code> можно вывести актуальный блок топа игроков, атрибут list_url может содержать ссылку на список всех игроков. С помощью шорткода <code>[chess_players_page]</code> можно вывести таблицу с поиском, сортировкой и пагинацией всех игроков. Также доступна возможность выгрузить таблицу рейтингов с динамикой (для админа за любой период). С помощью шорткода <code>[chess_month_ratings_files last="1|0"]</code> можно список таблиц рейтингов с динамикой за месяц доступных для скачивания пользователям, атрибут last отвечает за вывод только последнего файла, или всего списка (по умолчанию).
  * Requires PHP: 7.0
  * Requires at least: 4.7
 */
